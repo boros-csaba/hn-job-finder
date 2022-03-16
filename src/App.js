c6952ba3df6a93fab487065d1afc5e094b0ebeb2
Csaba Boros <boros.csaba94@gmail.com>
2022-03-16 20:43:29 +0200

    Load whoishiring submissions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [loadingMessage] = useState("Loading...");
+  const [isLoading] = useState(true);
+  const [loadingMessage, setLoadingMessage] = useState("Loading whoishiring submissions...");
+
+  useEffect(() => {
+    fetch('https://hacker-news.firebaseio.com/v0/user/whoishiring.json?print=pretty')
+      .then(response => response.json())
+      .then(data => {
+        setLoadingMessage("Done");
+        console.log(data)
+      });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {loadingMessage}
-        </p>
+        <p>{loadingMessage}</p>
       </header>
     </div>
   );
