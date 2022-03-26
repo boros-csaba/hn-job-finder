eb1d7fd426e104c9bdcbcc719843c3b7da83921a
Csaba Boros <boros.csaba94@gmail.com>
2022-03-26 20:23:20 +0200

    Add firebase config

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBebbBqw6s-AbbuzVd-tNoLYqMQzCFZ4Go",
+  authDomain: "hn-job-finder.firebaseapp.com",
+  projectId: "hn-job-finder",
+  storageBucket: "hn-job-finder.appspot.com",
+  messagingSenderId: "610166739966",
+  appId: "1:610166739966:web:fe876f654b807bb1cca6af"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
